a6c967e379f783200a124aa3029adab5fb5d1b57
maximsulim <cool.sulim@list.ru>
2025-06-21 11:39:37 +0300

    Initial Commit

---
+++ RevealCity/RevealCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevealCityApp.swift
+//  RevealCity
+//
+//  Created by Максим Сулим on 21.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct RevealCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
